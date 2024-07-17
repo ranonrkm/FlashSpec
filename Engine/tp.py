2c54dd01dc06e1bedf02dfd3a2b8c106fcadb650
jianc99 <jianchen0311@gmail.com>
2024-07-17 00:03:43 -0400

    Update tp.py

--- Engine/tp.py
+++ Engine/tp.py
@@ -55,7 +55,7 @@ def init_dist(draft_ranks=None):
     global_rank = _get_global_rank()
     world_size = _get_world_size()
     torch.cuda.set_device(global_rank)
-    dist.init_process_group(backend="nccl", rank=global_rank, world_size=world_size)
+    dist.init_process_group(backend="nccl")
     # global_group = dist.group.WORLD
     global_group = dist.new_group([0,1,2,3,4,5,6,7])
     if draft_ranks != None:
