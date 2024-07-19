c622717ae84438b31dbffe4bc7565e2d5a26a6b7
jianc99 <jianchen0311@gmail.com>
2024-07-18 22:36:42 -0400

    Update model.py

--- Engine/model.py
+++ Engine/model.py
@@ -184,7 +184,7 @@ class Attention(nn.Module):
 
         y = self.wo(y)
         if self.process_group != None:
-            dist.all_reduce(y, group = self.process_group)
+            dist.all_reduce(y)
         return y
 
 
@@ -199,7 +199,7 @@ class FeedForward(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         y = self.w2(F.silu(self.w1(x)) * self.w3(x))
         if self.process_group != None:
-            dist.all_reduce(y, group = self.process_group)
+            dist.all_reduce(y)
         return y
 
 
